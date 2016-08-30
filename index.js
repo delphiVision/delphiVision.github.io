e453ce9878b7177ea413c5f20a57ad6d8f41cc25
Akilesh Bapu <akilesh@berkeley.edu>
2016-08-30 00:03:37 -0700

    User Login attempt

---
+++ index.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+$('#login').click(function() {
+  authenticateUser($('#email').val(), $('#password').val())
+})
+});
+
+function authenticateUser(email, password) {
+  var serverURL = "http://ec2-54-218-78-162.us-west-2.compute.amazonaws.com/authenticateCompany"
+  var formData = new FormData();
+  var xmlhttp = new XMLHttpRequest();
+  formData.append("email", email);
+  formData.append("password", password);
+  xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          if (xmlhttp.responseText == "False") {
+            $('#incorrectPassword').innerText = "Incorrect Password";
+          } else {
+            $('#incorrectPassword').innerText = "Authenticated";
+          }
+      }
+  }
+  xmlhttp.open("POST", serverURL);
+  xmlhttp.send(formData);
+}
